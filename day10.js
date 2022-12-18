3569b10f24db9d4365c561406dc1512c6bb7c9e3
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-18 03:26:01 -0500

    Solve day 10 part 1

--- day10.js
+++ day10.js
@@ -1,5 +1,56 @@
+let cycleCounter = 0;
+let registerX = 1;
+let signalStrength = [];
+
+function executeInstruction(instruction) {
+    return parseInt(instruction);
+}
+
+function incrementCycle(){
+    cycleCounter++;
+    console.log(`Cycle: ${cycleCounter}, X: ${registerX}`);
+    switch(cycleCounter) {
+        case 20:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+        case 60:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+        case 100:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+        case 140:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+        case 180:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+        case 220:
+            signalStrength.push(registerX * cycleCounter);
+            break;
+    }
+}
+
 function part1(inputString) {
-    let solution = inputString;
+    let solution = 0;
+    let instructions = inputString.split(' ');
+    let currentInstruction;
+    cycleCounter = 0;
+    registerX = 1;
+    signalStrength = [];
+    while(instructions.length !== 0) {
+        currentInstruction = instructions.shift();
+        if(currentInstruction === "noop") { 
+            incrementCycle(); 
+        } else {
+            incrementCycle(); 
+            incrementCycle(); 
+            registerX += executeInstruction(instructions.shift());
+        }
+    }
+    signalStrength.forEach((signal) => {
+        solution += signal; 
+    });
     return solution;
 }
 
