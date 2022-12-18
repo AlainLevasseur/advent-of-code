80ab4ed6ba87589ffa534f552e42bf12e33e91de
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-18 02:36:52 -0500

    Solve day 9 part 2

--- day9.js
+++ day9.js
@@ -25,8 +25,7 @@ function moveTail(head, tail){
         if(verticalDistance > 0) {
             head[1] > tail[1] ? newTailPosition[1]++ : newTailPosition[1]--;
         }
-    }
-    if(verticalDistance > 1) {
+    } else if(verticalDistance > 1) {
         head[1] > tail[1] ? newTailPosition[1]++ : newTailPosition[1]--;
         if(horizontalDistance > 0) {
             head[0] > tail[0] ? newTailPosition[0]++ : newTailPosition[0]--;
@@ -58,12 +57,49 @@ function part1(inputString) {
             };
         }
     }
-
     return solution;
 }
 
+function displayRope(rope) {
+    const DIAGRAM_SIZE = 6;
+    let displayString = '';
+    for(let i = -DIAGRAM_SIZE; i < DIAGRAM_SIZE; i++) {
+        for(let j = -DIAGRAM_SIZE; j < DIAGRAM_SIZE; j++) {
+            let sameSpot = '';
+            rope.forEach((element, index) => {
+                sameSpot += element[0] === i && element[1] === j ? `${index}` : '';
+            });
+            displayString += sameSpot.length > 0 ? sameSpot[0] : '*';
+        }
+        displayString += '\n';
+    }
+    console.log(displayString);
+}
+
 function part2(inputString) {
-    let solution = inputString;
+    let solution = 1;
+    let moveList = parseMoves(inputString);
+    let rope = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+    const HEAD = 0;
+    const TAIL = 9;
+    let tailHistory = [`${rope[TAIL]}`];
+
+    for(let move = 0; move < moveList.length; move += 2) {
+        let moveDirection = moveList[move];
+        let moveAmount = moveList[move + 1];
+        for(let step = 0; step < moveAmount; step++) {
+            rope[HEAD] = moveHead(rope[HEAD], moveDirection, 1);
+            for(let knot = 1; knot < rope.length; knot++){
+                rope[knot] = moveTail(rope[knot - 1], rope[knot]);
+                //displayRope(rope);
+            }
+            if(uniqueTailMove(rope[TAIL], tailHistory)) {
+                solution++;
+                tailHistory.push(`${rope[TAIL]}`);
+                // console.log(tailHistory);
+            };
+        }
+    }
     return solution;
 }
 
