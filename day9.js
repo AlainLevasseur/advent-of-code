19eb8aa0060cf39d77dfd15afcb6f87708de60c5
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-18 01:20:17 -0500

    Solve day 9 part 1

--- day9.js
+++ day9.js
@@ -1,5 +1,64 @@
+function parseMoves(string) {
+    return string.split(' ');
+}
+
+function moveHead(position, direction, amount) {
+    switch(direction) {
+        case 'R':
+            return [position[0] + amount, position[1]];
+        case 'L':
+            return [position[0] - amount, position[1]];
+        case 'U':
+            return [position[0], position[1] + amount];
+        case 'D':
+            return [position[0], position[1] - amount];
+    }
+}
+
+function moveTail(head, tail){
+    let horizontalDistance = Math.abs(tail[0] - head[0]);
+    let verticalDistance = Math.abs(tail[1] - head[1]);
+    let newTailPosition = [tail[0], tail[1]];
+
+    if(horizontalDistance > 1) {
+        head[0] > tail[0] ? newTailPosition[0]++ : newTailPosition[0]--;
+        if(verticalDistance > 0) {
+            head[1] > tail[1] ? newTailPosition[1]++ : newTailPosition[1]--;
+        }
+    }
+    if(verticalDistance > 1) {
+        head[1] > tail[1] ? newTailPosition[1]++ : newTailPosition[1]--;
+        if(horizontalDistance > 0) {
+            head[0] > tail[0] ? newTailPosition[0]++ : newTailPosition[0]--;
+        }
+    }
+    return newTailPosition;
+}
+
+function uniqueTailMove(tailPosition, tailHistory) {
+    return !tailHistory.includes(`${tailPosition}`);
+}
+
 function part1(inputString) {
-    let solution = inputString;
+    let solution = 1;
+    let moveList = parseMoves(inputString);
+    let headPosition = [0,0];
+    let tailPosition = [0,0];
+    let tailHistory = [`${tailPosition}`];
+
+    for(let move = 0; move < moveList.length; move += 2) {
+        let moveDirection = moveList[move];
+        let moveAmount = moveList[move + 1];
+        for(let step = 0; step < moveAmount; step++) {
+            headPosition = moveHead(headPosition, moveDirection, 1);
+            tailPosition = moveTail(headPosition, tailPosition);
+            if(uniqueTailMove(tailPosition, tailHistory)) {
+                solution++;
+                tailHistory.push(`${tailPosition}`);
+            };
+        }
+    }
+
     return solution;
 }
 
